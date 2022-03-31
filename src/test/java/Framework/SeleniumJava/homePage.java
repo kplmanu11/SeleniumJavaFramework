053b835e7b178a3f29602e6447cf2d0a04431128
Neupane <kapil.neupane@cotiviti.com>
2022-03-31 14:59:24 +0545

    project converted to testng and some minor changes

--- src/test/java/Framework/SeleniumJava/homePage.java
+++ src/test/java/Framework/SeleniumJava/homePage.java
@@ -3,6 +3,8 @@ package Framework.SeleniumJava;
 import java.io.IOException;
 
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -11,31 +13,41 @@ import pageObjects.loginPage;
 import resources.base;
 
 public class homePage extends base {
+	
+	@BeforeTest
+	public void initialize() throws IOException {
+		driver = initializeDriver();
+		driver.get(prop.getProperty("url"));
+	}
 
 	@Test(dataProvider = "getData")
 	public void invokingDriver(String username, String password, String text) throws IOException, InterruptedException {
-		driver = initializeDriver();
-		driver.get(prop.getProperty("url"));
+		
 
 		landingPage lpg = new landingPage(driver);
 		Thread.sleep(3000);
 		Assert.assertEquals(lpg.titleText().getText(), "FEATURED COURSES");
 		Assert.assertTrue(lpg.navBarAll().isDisplayed());
-		System.out.println(lpg.navBarAll().isDisplayed());
+		//System.out.println(lpg.navBarAll().isDisplayed());
 		lpg.noThankYouText().click();
 		lpg.loginClick().click();
 
 		loginPage lp = new loginPage(driver);
 		lp.EmailAddress().sendKeys(username);
 		lp.Password().sendKeys(password);
-		System.out.print(text);
+		//System.out.print(text);
 		lp.LoginButton().click();
 
+	}
+	
+	@AfterTest
+	public void TearDown() {
+		driver.close();
 	}
 
 	@DataProvider
 	public Object[][] getData() {
-		Object[][] obj = new Object[2][3];
+		Object[][] obj = new Object[1][3];
 
 		// 0th row
 		obj[0][0] = "abc@gmail.com";
@@ -43,9 +55,10 @@ public class homePage extends base {
 		obj[0][2] = "Restricted User";
 
 		// 1st row
-		obj[1][0] = "xyz@gmail.com";
-		obj[1][1] = "password";
-		obj[1][2] = "Non-Restricted User";
+		/*
+		 * obj[1][0] = "xyz@gmail.com"; obj[1][1] = "password"; obj[1][2] =
+		 * "Non-Restricted User";
+		 */
 
 		return obj;
 	}
